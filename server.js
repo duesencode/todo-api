7c51b72bee5e3bc6ff785523f054618960bd3a55
Dirk Fritz <dirk.fritz@arconsis.com>
2018-06-04 00:09:13 +0200

    Adds GET /todos route

--- server.js
+++ server.js
@@ -12,9 +12,17 @@ app.use(bodyParser.json());
 app.post('/todos', (req, res) => {
     var todo = new Todo(req.body)
     todo.save().then((doc) => {
-        res.send(doc)
-    }).catch((e) => {
-        res.status(400).send(e)
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({ todos });
+    }, (e) => {
+        res.status(400).send(e);
     });
 });
 
